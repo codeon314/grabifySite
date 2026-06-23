39a5fc3e3564e31aba5ace37298071a79b89cbd4
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-23 02:39:58 -0500

    Delete [[path]].js

--- functions/[[path]].js
+++
@@ -1,6 +0,0 @@
-// functions/[[path]].js
-import serverHandler from '../dist/server/entry.mjs';
-
-export const onRequest = (context) => {
-  return serverHandler(context.request, context.env, context);
-};
