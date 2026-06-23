1075c45db8bc935af6b2f59a54efb87262f343dd
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-23 07:15:50 -0500

    ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://grabify.mastercodeon.dev',
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  trailingSlash: 'ignore'
 });
