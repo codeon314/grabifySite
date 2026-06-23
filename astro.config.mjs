cdab9085cdfd76429bc6a28e2fdd2ad70dca8030
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-23 07:32:57 -0500

    Fuck me

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://grabify.mastercodeon.dev',
   output: 'server',
   adapter: cloudflare(),
   trailingSlash: 'ignore'
