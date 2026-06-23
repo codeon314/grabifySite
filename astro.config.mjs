947102e3505816d8f1c97931816f0d1b092473a2
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-23 07:06:03 -0500

    F

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  site: 'https://grabify.mastercodeon.dev',
   output: 'server',
   adapter: cloudflare()
 });
