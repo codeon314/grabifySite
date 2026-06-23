78c3f9cb0fd12f7465caee90c43acfb82b14105d
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-23 06:11:40 -0500

    Even more fixes smh

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    mode: 'directory'
-  })
+  adapter: cloudflare() // Removing mode: 'directory' forces Astro to generate a _worker.js file
 });
