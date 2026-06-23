0676928eec03bd4c315241a2d120719f80744c02
0% [█ █ █ █ █ █ █ █ █ █] 100% <lild31415@gmail.com>
2026-06-23 18:02:55 -0500

    Fixed push notifs for mobile

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  // Focus the window if the user clicks the notification
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(windowClients => {
+      if (windowClients.length > 0) {
+        windowClients[0].focus();
+      }
+    })
+  );
+});
